2525553b8cbdbb6a035f4788e9181ce3b3d6e49c
Pavel Vanecek <pavel.vanecek@merck.com>
2017-12-26 16:13:03 +0100

    ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [ "warn", 2 ],
+        "linebreak-style": [ "error", "unix" ],
+        "quotes": [ "warn", "single" ],
+        "semi": [ "warn", "never" ],
+        "no-trailing-spaces": "warn",
+        "no-unused": "warn"
+    }
+};
